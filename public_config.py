6d1f10192a87788baa0effd98d1c54292e2abf79
zhou <939889912@qq.com>
2025-12-17 03:18:53 +0000

    fix: 方便测试

--- public_config.py
+++ public_config.py
@@ -1,6 +1,8 @@
 # yolo_yaml= "yolov8n.yaml"
-yolo_yaml= "yolov8-pde.yaml"
+# yolo_yaml= "yolov8-wiou.yaml"
+# yolo_yaml= "yolov8-pde.yaml"
+yolo_yaml= "yolov5.yaml"
 yolo_pth= "yolo11n.pt"
-# dataset_yaml = "coco8.yaml"
-dataset_yaml = "NEU-DET.yaml"
+dataset_yaml = "coco8.yaml"
+# dataset_yaml = "NEU-DET.yaml"
 default_cfg_yaml = "cfg/default.yaml"   # 前处理啥的全在这
